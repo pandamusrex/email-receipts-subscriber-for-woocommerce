313e2bb3ac0768dafea3fcd5b0db9daae81c07ce
pandamusrex <pandamusrex@gmail.com>
2025-08-16 10:05:44 -0700

    Start adding order linking meta box

--- includes/notification-post-type.php
+++ includes/notification-post-type.php
@@ -20,14 +20,15 @@ class PandamusRex_Email_Receipts_Notification_Post_Type {
 
     public function __construct() {
         add_action( 'init', [ $this, 'init' ] );
+        add_action( 'add_meta_boxes', [ $this, 'add_meta_boxes' ] );
     }
 
     public function init() {
         register_post_type( 'pandamusrex_mailnote',
             array(
                 'labels'       => array(
-                    'name'          => __('Email Notifications', 'textdomain'),
-                    'singular_name' => __('Email Notification', 'textdomain'),
+                    'name'          => __('Payments Webhook Notifications', 'textdomain'),
+                    'singular_name' => __('Payments Webhook Notification', 'textdomain'),
                 ),
                 'public'              => false,
                 'publicly_queryable'  => false,
@@ -37,6 +38,24 @@ class PandamusRex_Email_Receipts_Notification_Post_Type {
                 'show_in_menu'        => true,
             )
 	    );
+    }
+
+    public function add_meta_boxes() {
+        add_meta_box( 'pmts_wbhk_lstn_sectionid', __( 'Related Order' ), [ $this, 'meta_box' ], 'post', 'side', 'high' );
+    }
+
+    public function meta_box( $post) {
+        $linked_order_ID = get_post_meta( $post->ID, '_linked_order_id', true );
+        if ( empty( $linked_order_ID ) ) {
+            echo "<p>Choose an order to link this payment to:";
+            echo "</p>";
+        } else {
+            echo "<p>Linked order: ";
+            echo get_edit_post_link( $post );
+            echo "<br/>";
+            echo "<a href='#'>Unlink</a>";
+            echo "</p>";
+        }
     }
 }
 
