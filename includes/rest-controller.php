ececad0d9c2379eae2bd58e8cb4a2b36ada53cef
pandamusrex <pandamusrex@gmail.com>
2025-08-16 08:59:26 -0700

    Start adding POSTable endpoint

--- includes/rest-controller.php
+++ includes/rest-controller.php
@@ -19,10 +19,33 @@ class PandamusRex_Email_Receipts_Rest_Controller extends \WP_REST_Controller {
                 'permission_callback' => [ $this, 'check_permission' ],
             ]
         );
+
+        register_rest_route(
+            $this->namespace,
+            '/' . $this->rest_base . '/receipt',
+            [
+                'methods'             => \WP_REST_Server::CREATABLE,
+                'callback'            => [ $this, 'post_receipt' ],
+                'permission_callback' => [ $this, 'check_permission' ],
+            ]
+        );
     }
 
     public function get_silence() {
         return 'Silence is golden.';
+    }
+
+    public function post_receipt( $request ) {
+
+        $body = wp_kses_post( $request->get_body() );
+
+        $id = wp_insert_post( array(
+            'post_title'  => 'random',
+            'post_type'   => 'pandamusrex_mailnote',
+            'post_content' => $body
+        ) );
+
+        return new WP_REST_Response( true, 200 );
     }
 
     public function check_permission() {
